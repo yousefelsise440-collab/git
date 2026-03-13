840d22cb4f791bf535572b3a0616b90f8364af1a
Yousef Mohamed <yousefelsise440@gmail.com>
2026-03-13 22:56:51 +0200

    hospital

---
+++ MedicalRecord.java/MedicalRecord.java
@@ -0,0 +1,35 @@
+package MedicalRecord.java;
+
+public class MedicalRecord {
+    private String recordID;
+    private String diagnosis;
+    private String prescription;
+    private String data;
+    private String doctorName;
+    public MedicalRecord(String recordID, String diagnosis, String prescription, String data, String doctorName) {
+        this.recordID = recordID;
+        this.diagnosis = diagnosis;
+        this.prescription = prescription;
+        this.data = data;
+        this.doctorName = doctorName;
+    }
+    public void printInfo() {
+        System.out.println("Record ID: " + recordID);
+        System.out.println("Diagnosis: " + diagnosis);
+        System.out.println("Prescription: " + prescription);
+        System.out.println("Data: " + data);
+        System.out.println("Doctor Name: " + doctorName);
+    }
+    public String getRecordID() {
+        return recordID;
+    }public String getDiagnosis() {
+        return diagnosis;
+    }public String getPrescription() {
+        return prescription;
+    }public String getData() {
+        return data;
+    }public String getDoctorName() {
+        return doctorName;
+    }
+
+}
--- pu.java
+++ pu.java
@@ -1,6 +1,2 @@
 
-public class pu {
-    public static void main(String[] args) {
-        System.out.println("Hello from pu!");
-    }youserrx
 }
