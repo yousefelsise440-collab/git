c755541bac90c956f7ddf1d96ae0da290f53c894
Yousef Mohamed <yousefelsise440@gmail.com>
2026-03-14 14:27:04 +0200

    UPDATE MEDICAL

--- MedicalRecord.java/hospital/MedicalRecord.java
+++ MedicalRecord.java/hospital/MedicalRecord.java
@@ -1,18 +1,34 @@
+package hospital;
+interface Printable {
+    void printInfo();
+}
+class Document {
+protected String recordID;
+public Document(String recordID) {
+    this.recordID = recordID;
+    }
 
-
-public class MedicalRecord {
-    private String recordID;
-    private String diagnosis;
+    
+}
+public class MedicalRecord extends Document implements Printable {
+private String diagnosis;
     private String prescription;
     private String date;
     private String doctorName;
     public MedicalRecord(String recordID, String diagnosis, String prescription, String date, String doctorName) {
-        this.recordID = recordID;
+        super (recordID);
         this.diagnosis = diagnosis;
         this.prescription = prescription;
         this.date = date;
         this.doctorName = doctorName;
     }
+     super(recordID); 
+     this.diagnosis = diagnosis;
+     this.prescription = prescription;
+        this.date = date;
+        this.doctorName = doctorName;
+
+   @Override
     public void printInfo() {
         System.out.println("Record ID: " + recordID);
         System.out.println("Diagnosis: " + diagnosis);
@@ -26,7 +42,7 @@ public class MedicalRecord {
         return diagnosis;
     }public String getPrescription() {
         return prescription;
-    }public String getData() {
+    }public String getDate() {
         return date;
     }public String getDoctorName() {
         return doctorName;
