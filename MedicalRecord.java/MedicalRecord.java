249ecedb3c6afc75a5d1072c2bdae91454669771
Yousef Mohamed <yousefelsise440@gmail.com>
2026-03-14 13:17:31 +0200

    update medicalrecord

--- MedicalRecord.java/MedicalRecord.java
+++ MedicalRecord.java/MedicalRecord.java
@@ -6,18 +6,18 @@ public class MedicalRecord {
     private String prescription;
     private String date;
     private String doctorName;
-    public MedicalRecord(String recordID, String diagnosis, String prescription, String data, String doctorName) {
+    public MedicalRecord(String recordID, String diagnosis, String prescription, String date, String doctorName) {
         this.recordID = recordID;
         this.diagnosis = diagnosis;
         this.prescription = prescription;
-        this.date = data;
+        this.date = date;
         this.doctorName = doctorName;
     }
     public void printInfo() {
         System.out.println("Record ID: " + recordID);
         System.out.println("Diagnosis: " + diagnosis);
         System.out.println("Prescription: " + prescription);
-        System.out.println("Data: " + data);
+        System.out.println("Date: " + date);
         System.out.println("Doctor Name: " + doctorName);
     }
     public String getRecordID() {
